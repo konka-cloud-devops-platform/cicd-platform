c7007c5961fd87f7f3e2f54ba675a92fac3f4312
siva <sivaram0434@gmail.com>
2026-01-17 22:51:49 +0530

    update the trovy src

--- src/org/security/TrivyScan.groovy
+++ src/org/security/TrivyScan.groovy
@@ -1,7 +1,18 @@
+// package org.security
+
+// class TrivyScan {
+//   static void scan(String image) {
+//     sh "trivy image --config trivy.yaml ${image}"
+//   }
+// }
 package org.security
 
 class TrivyScan {
-  static void scan(String image) {
-    sh "trivy image --config trivy.yaml ${image}"
-  }
+    static void scan(def steps, String image) {
+        steps.sh """
+          trivy image \
+          --config trivy.yaml \
+          ${image}
+        """
+    }
 }
